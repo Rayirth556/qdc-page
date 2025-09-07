9578832d8cb00c58e50e6b14ac20cfadbdafcb41
Rayirth556 <rayirthmisra25@gmail.com>
2025-09-08 01:54:02 +0530

    Fix CSS loading issue: downgrade to Tailwind v3 and fix file paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],
+  content: [
+    "./public/**/*.{html,js}",
+    "./src/**/*.{html,js}",
+    "./public/index.html"
+  ],
   theme: {
     extend: {},
   },
